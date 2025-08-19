b4390e77f3d32c55f8022a047e7e8b8a91ffb8f1
Archit Kumar <architxkumar@proton.me>
2025-08-19 08:56:11 +0530

    refactor: extract `RetrieveRequestID` to `RequestIDMiddleware` for combining concerns

--- backend/middleware/request_id.go
+++ backend/middleware/request_id.go
@@ -3,7 +3,8 @@ package middleware
 import (
 	"net/http"
 
-	uuid2 "github.com/google/uuid"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 )
 
@@ -13,10 +14,25 @@ const requestIDKey key = "requestID"
 
 func RequestIDMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestId := uuid2.New()
+		requestId := uuid.New()
 
 		ctx := context.WithValue(r.Context(), requestIDKey, requestId)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+
+func RetrieveRequestID(r *http.Request) (requestID uuid.UUID) {
+	value := r.Context().Value(requestIDKey)
+	if value != nil {
+		reqID, ok := value.(uuid.UUID)
+		if !ok {
+			logrus.Warn("Type assertion failure on 'requestID'")
+		} else {
+			requestID = reqID
+		}
+	} else {
+		logrus.Warn("Missing requestID key")
+	}
+	return
 }
