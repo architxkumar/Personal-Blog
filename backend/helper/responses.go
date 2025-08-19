ee991bb73454dd40e7dbd63ea787fbc5202e3367
Archit Kumar <architxkumar@proton.me>
2025-08-19 18:47:58 +0530

    refactor: update response builder helper functions
    - Update Error Message in `InternalServerError` and `UnauthorizedRequest` Payload Builder
    - Update order of setting headers inside `ErrorResponseWriter`

--- backend/helper/responses.go
+++ backend/helper/responses.go
@@ -34,7 +34,7 @@ func BuildBadRequestPayload(requestUuid uuid.UUID, errorDetail *model.ErrorDetai
 func BuildInternalServerErrorPayload(requestUuid uuid.UUID, errorDetail *model.ErrorDetails) ([]byte, error) {
 	responseBytes, err := json.Marshal(model.ErrorResponse{Error: model.APIError{
 		Code:    "INTERNAL_SERVER_ERROR",
-		Message: "Internal Server Error",
+		Message: "Something went wrong. Please try again later.",
 		Status:  http.StatusInternalServerError,
 		TraceID: requestUuid.String(),
 		Details: errorDetail,
@@ -47,7 +47,7 @@ func BuildInternalServerErrorPayload(requestUuid uuid.UUID, errorDetail *model.E
 
 func BuildUnauthorizedRequestPayload(requestUuid uuid.UUID, errorDetail *model.ErrorDetails) ([]byte, error) {
 	responseBytes, err := json.Marshal(model.ErrorResponse{Error: model.APIError{
-		Code:    "INVALID_CREDENTIALS",
+		Code:    "UNAUTHENTICATED",
 		Message: "Authentication Token is missing, invalid or expired. Please log in again.",
 		Status:  http.StatusUnauthorized,
 		TraceID: requestUuid.String(),
@@ -61,13 +61,14 @@ func BuildUnauthorizedRequestPayload(requestUuid uuid.UUID, errorDetail *model.E
 }
 
 func ErrorResponseWriter(w http.ResponseWriter, statusCode int, requestID uuid.UUID, payloadBuilder func(uuid.UUID, *model.ErrorDetails) ([]byte, error), errorDetails *model.ErrorDetails) {
+	SetResponseHeaders(w)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(statusCode)
 	responseBodyBytes, err := payloadBuilder(requestID, errorDetails)
 	if err != nil {
 		logrus.Error("Error creating response Body: ", err)
 		return
 	}
-	SetResponseHeaders(w)
 	_, err = w.Write(responseBodyBytes)
 	if err != nil {
 		logrus.Error("Error writing response: ", err)
