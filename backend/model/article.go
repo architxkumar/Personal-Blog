6d98d21f6cd26c9caef606c004278fa15a4ac99c
Archit Kumar <architxkumar@proton.me>
2025-08-19 08:37:10 +0530

    refactor: update `article` schema to include `title` field

--- backend/model/article.go
+++ backend/model/article.go
@@ -4,6 +4,12 @@ import "github.com/google/uuid"
 
 type Article struct {
 	Id      uuid.UUID `json:"id"`
+	Title   string    `json:"title"`
 	Content string    `json:"content"`
 	Date    string    `json:"date"`
+}
+
+type ArticlePreviewDTO struct {
+	Id    uuid.UUID `json:"id"`
+	Title string    `json:"title"`
 }
